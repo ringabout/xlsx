47c8b4f3b0ad455794eeda507404bc781f56fe71
flywind <xzsflywind@gmail.com>
2020-01-03 18:19:55 +0800

    update

--- tests/test_read_excel.nim
+++ tests/test_read_excel.nim
@@ -2,15 +2,15 @@ import unittest
 
 import xlsx
 
-suite "read excel with types":
+suite "Read excel with types":
   let sheetName = "Sheet1"
 
-  test "read excel with type int":
+  test "Read excel with type int":
     let data = readExcel[int]("tests/test_read_excel.xlsx", sheetName,
         skipHeaders = false)
     check(data.data == @[1, 4, 7, 9, 4, 7, 0, 3, 12, 54, 24, 887])
 
-  test "read excel with type float":
+  test "Read excel with type float":
     let data = readExcel[float]("tests/test_read_excel.xlsx", sheetName,
         skipHeaders = false)
     check(data.data == @[1.0, 4.0, 7.0, 9.0, 4.0, 7.0, 0.0, 3.0,
