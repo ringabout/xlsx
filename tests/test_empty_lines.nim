2119b58deb24b7e10e8a415b4a4e84e2162751fa
flywind <xzsflywind@gmail.com>
2020-01-26 15:11:02 +0800

    fixes

--- tests/test_empty_lines.nim
+++ tests/test_empty_lines.nim
@@ -5,7 +5,7 @@ import xlsx
 suite "skip empty lines":
   let sheetName = "Cx"
   test "parse Excel and skip empty lines":
-    let data = parseExcel("tests/test_empty_lines.xlsx", header = false, trailing = true)
+    let data = parseExcel("tests/test_empty_lines.xlsx", header = false, trailingRows = true)
     let rows = data[sheetName].toSeq(false)
     check rows.len == 10
     check rows[0].len == 4
