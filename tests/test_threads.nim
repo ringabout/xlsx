cb9411733e1ef519b981cfb631ba2b094664823d
ringabout <43030857+ringabout@users.noreply.github.com>
2024-11-24 20:01:58 +0800

    Update test_threads.nim

--- tests/test_threads.nim
+++ tests/test_threads.nim
@@ -10,21 +10,20 @@ when compileOption("threads"):
     return data[sheetName].data == @["name", "grade", "age",
       "simon", "", "14", "tom", "87", "34"]
 
-  when not defined(gcArc) and not defined(gcOrc):
     suite "Test parse Excel with threads":
       let sheetName = "Sheet1"
   
-      test "Parse Excel with threads":
-        when compileOption("mm", "arc"):
-          let fv = spawn(parseExcel("tests/test.xlsx"))
-          let data = ^fv
-          check(data[sheetName].data == @["name", "grade", "age",
-            "simon", "", "14", "tom", "87", "34"])
-        else:
-          let fv = spawn(checkData(sheetName))
-          check(^fv)
-  
-        test "Get all sheet names with threads":
-          let fv = spawn(parseAllSheetName("tests/test.xlsx"))
-          let data = ^fv
-          check(data == @["Sheet1", "Sheet2"])
+    test "Parse Excel with threads":
+      when defined(gcArc) or defined(gcOrc):
+        let fv = spawn(parseExcel("tests/test.xlsx"))
+        let data = ^fv
+        check(data[sheetName].data == @["name", "grade", "age",
+          "simon", "", "14", "tom", "87", "34"])
+      else:
+        let fv = spawn(checkData(sheetName))
+        check(^fv)
+
+      test "Get all sheet names with threads":
+        let fv = spawn(parseAllSheetName("tests/test.xlsx"))
+        let data = ^fv
+        check(data == @["Sheet2", "Sheet1"])
